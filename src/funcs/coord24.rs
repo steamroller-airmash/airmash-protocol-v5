70de8e73185d60e6d7138e8ba5a431c611dff8d3
steamroller-airmash <38710707+steamroller-airmash@users.noreply.github.com>
2018-12-15 14:12:05 -0500

    Fix deserialization bug within coord24

--- src/funcs/coord24.rs
+++ src/funcs/coord24.rs
@@ -12,3 +12,21 @@ pub fn serialize(val: &Distance, ser: &mut Serializer) -> Result<(), SerializeEr
 pub fn deserialize<'de>(de: &mut Deserializer<'de>) -> Result<Distance, DeserializeError> {
 	Ok(((((uint24::deserialize(de)? as i32) - SHIFT) as f32) / MULT).into())
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn roundtrip_0() {
+		let pos = Distance::new(0.0);
+		let mut ser = Serializer::new();
+		serialize(&pos, &mut ser).unwrap();
+		let mut de = Deserializer::new(&mut ser.bytes);
+		let new = deserialize(&mut de).unwrap();
+
+		eprintln!("new: {}\npos: {}", new, pos);
+
+		assert!(new == pos);
+	}
+}
--- src/funcs/uint24.rs
+++ src/funcs/uint24.rs
@@ -6,8 +6,36 @@ pub fn serialize(val: u32, ser: &mut Serializer) -> Result<(), SerializeError> {
 	(val as u8).serialize(ser)
 }
 pub fn deserialize<'de>(de: &mut Deserializer<'de>) -> Result<u32, DeserializeError> {
-	let hi = u16::deserialize(de)?;
-	let lo = u8::deserialize(de)?;
+	let hi = u16::deserialize(de)? as u32;
+	let lo = u8::deserialize(de)? as u32;
 
-	Ok(((hi << 8) as u32) | (lo as u32))
+	Ok((hi << 8) | lo)
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn roundtrip_4325381() {
+		let val = (1 << 22) + (1 << 15) + 5;
+		let mut ser = Serializer::new();
+		serialize(val, &mut ser).unwrap();
+		let mut de = Deserializer::new(&ser.bytes);
+		let new = deserialize(&mut de).unwrap();
+
+		assert_eq!(val, new);
+	}
+
+	#[test]
+	fn serialize_4325381() {
+		let val = (1 << 22) + (1 << 15) + 5;
+		let mut ser = Serializer::new();
+		serialize(val, &mut ser).unwrap();
+
+		let buf = ser.bytes;
+		assert_eq!(buf[0], 128);
+		assert_eq!(buf[1], 64);
+		assert_eq!(buf[2], 5);
+	}
 }
